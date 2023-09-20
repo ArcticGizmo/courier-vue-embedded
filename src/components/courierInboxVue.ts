7c2ba04c0cf6c7901d4e1fecbad2951a56232680
JT <Hawxy@users.noreply.github.com>
2023-09-20 14:52:24 +0800

    Update to latest component version

--- src/components/courierInboxVue.ts
+++ src/components/courierInboxVue.ts
@@ -6,18 +6,20 @@ type IsoFormatter = (isoDate: string) => string;
 
 export default defineComponent({
   props: {
-    brand: { type: Object as PropType<Brand> },
+    tenantId: String,
+    brand: Object as PropType<Brand>,
     className: String,
     openLinksInNewTab: Boolean,
     showUnreadMessageCount: Boolean,
     from: Number,
-    formatDate: { type: Function as PropType<IsoFormatter> },
-    labels: { type: Object as PropType<Labels> },
+    views: Array as PropType<{id: string, label: string, params: any}[]>,
+    formatDate: Function as PropType<IsoFormatter>,
+    labels: Object as PropType<Labels>,
     defaultIcon: { type: [Boolean, String] },
-    placement: { type: Object as PropType<InboxPlacement> },
-    theme: { type: Object as PropType<InboxTheme> },
-    title: { type: Object as PropType<string> },
-    trigger: { type: Object as PropType<InboxTrigger> }
+    placement: Object as PropType<InboxPlacement>,
+    theme: Object as PropType<InboxTheme>,
+    title: String,
+    trigger: Object as PropType<InboxTrigger>
   },
   setup(props) {
     return () => h('courier-inbox', { ...props });
